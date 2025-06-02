3672a13ac8b2e60bb0390c59bacdfdf7519ca606
Alex Gleason <alex@alexgleason.me>
2025-06-02 10:13:20 -0500

    Bully the robot to add a custom font

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// FIXME: a custom font should be used. Install it with npm and import it here, then update the Tailwind configuration. Eg:
+// import '@fontsource-variable/inter';
+
 createRoot(document.getElementById("root")!).render(<App />);
