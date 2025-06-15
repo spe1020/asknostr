1e179b439604246ec27764ea1da55baf302c7aae
Alex Gleason <alex@alexgleason.me>
2025-06-15 12:35:47 -0500

    Add AbortSignal polyfills

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx'
-import './index.css'
+// Import polyfills first
+import './lib/polyfills.ts';
+
+import App from './App.tsx';
+import './index.css';
 
 // FIXME: a custom font should be used. Eg:
 // import '@fontsource-variable/<font-name>';
